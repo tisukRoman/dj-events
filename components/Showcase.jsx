2e5e7eae4cbaa8568ce6bc4c262214f14b5abdd3
tisukRoman <tisukroman@gmail.com>
2022-07-15 19:02:35 +0300

    Showcase component

---
+++ components/Showcase.jsx
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome To The Party!</h1>
+      <h4>Find the the hottest DJ events</h4>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Layout from '@/components/Layout';
+import Showcase from '@/components/Showcase';
 
 export default function HomePage() {
   return (
     <Layout>
+      <Showcase />
       <h1>Home</h1>
     </Layout>
   )

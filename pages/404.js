67d1d9169fce113dd51d272897489a6c70e63368
tisukRoman <tisukroman@gmail.com>
2022-07-15 18:09:43 +0300

    imports refactored

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
-import styles from '../styles/404.module.css';
 import { FaExclamationTriangle } from "react-icons/fa";
-import Layout from '../components/Layout';
+import styles from '@/styles/404.module.css';
+import Layout from '@/components/Layout';
 
 export default function DefaultPage() {
     return (

78b2d90933d6a312944cdc782d39bdbb2354dc49
tisukRoman <tisukroman@gmail.com>
2022-07-27 14:16:28 +0300

    updated getImgURL helper

--- helpers/getImgUrl.js
+++ helpers/getImgUrl.js
@@ -1,6 +1,11 @@
 export const getImgUrl = (image, size, defaultURL) => {
   if (image?.data?.attributes?.formats?.[size]) {
     return image.data.attributes.formats[size].url;
+  } else if (image?.data?.attributes?.formats?.medium) {
+    return image.data.attributes.formats.medium.url;
+  } else if (image?.data?.attributes?.formats?.small) {
+    return image.data.attributes.formats.medium.url;
+  } else {
+    return defaultURL;
   }
-  return defaultURL;
 };
